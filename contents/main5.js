f6883f936f384e39c4048551e1b04e9d32280010
ei0410 <chronolavos0124@gmail.com>
2018-09-15 09:10:55 +0000

    Add motorcontrol

--- contents/main5.js
+++ contents/main5.js
@@ -46,6 +46,37 @@ document.getElementById('motor_off').addEventListener('click', function(e){
     });
 });
 
+var vel = new ROSLIB.Topic({
+    ros : ros, 
+    name : '/cmd_vel',
+    messageType : 'geometry_msgs/Twist'
+});
+
+function pubMotorValues(){
+    fw = document.getElementById('vel_fw');
+    rot = document.getElementById('vel_rot');
+
+    fw = parseInt(fw)*0.001;
+    rot = 3.141592*parseInt(rot)/180;
+    v = new ROSLIB.Message({linear:{x:fw,y:0,z:0}, angular:{x:0, y:0, z:rot}});
+    vel.publish(v);
+}
+
+document.getElementById('touchmotion').addEventListener('click', function(e){
+    rect = document.getElementById('touchmotion');
+    x = e.pageX - rect.left - window.pageXOffset;
+    y = e.pageY - rect.top - window.pageYOffset;
+
+    vel_fw = (rect.height/2 - y)*3;
+    vel_rot = rect.width/2 - x;
+    document.getElementById('vel_fw');
+    document.getElementById('vel_rot');
+    //$('#vel_fw').html(parseInt(vel_fw));
+    //$('#vel_rot').html(parseInt(vel_rot));
+});
+
+setInterval(pubMotorValues, 100);
+
 document.getElementById('camstream').data = 'http://'
     + location.hostname
     + ':10000/stream?topic=/cv_camera_node/image_raw';
